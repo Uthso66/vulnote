1f6a74f28a6c1e71af0a212a9078310fc204eb84
uthso66 <tarikulislam.uthso.3966@gmail.com>
2026-01-03 20:49:39 +0600

    :tada: :shield: Initial commit: Architecting the Vulnote foundation

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main className="flex gap-6 p-6">
+      <div className="flex-1">
+        <h1>VulnNote</h1>
+        <p>Turn raw vulnerability notes into clean reports.</p>
+      </div>
+
+      <div className="flex-1"></div>
+    </main>
+  );
+}
